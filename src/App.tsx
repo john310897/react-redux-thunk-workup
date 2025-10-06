9f1540323a6700eb3d532a77190c988b44e26de4
John J <john310897@gmail.com>
2025-10-06 16:11:36 +0530

    updated ui added condetional rendering

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,34 @@ import { getUserData } from "./reducers"
 import { useSelector, useDispatch } from "react-redux"
 
 const App = () => {
-  const dispatch: any = useDispatch();
-  const userStore: any = useSelector(data => data)
+	const dispatch: any = useDispatch();
+	const userStore: any = useSelector(data => data)
+	const statusColor: any = {
+		'PENDING': 'orange',
+		'IDLE': 'red',
+		'SUCCESS': 'green'
+	}
 
-  useEffect(() => {
-    console.log(userStore)
+	useEffect(() => {
+		console.log(userStore)
 
-  }, [])
+	}, [])
 
-  const handleButtonClick = () => {
-    console.log(userStore)
-    dispatch(getUserData())
-  }
-  return (
-    <>
-      <button onClick={handleButtonClick}>get updated state</button>
-      <br /><br />
-      <button>
-        <p>dispatch api status: <b>{userStore?.data?.status}</b></p>
-      </button>
-    </>
-  )
+	const handleButtonClick = () => {
+		console.log(userStore)
+		dispatch(getUserData())
+	}
+	return (
+		<>
+			<div className="main_container">
+				<div className="main_dispatch_control">
+					<button onClick={handleButtonClick}>update dispatch status</button>
+				</div>
+				<div className="main_dispatch_status" style={{ 'background': statusColor[userStore?.data?.status] }}>
+					<p>{userStore?.data?.status}</p>
+				</div>
+			</div>
+		</>
+	)
 }
 export default App
